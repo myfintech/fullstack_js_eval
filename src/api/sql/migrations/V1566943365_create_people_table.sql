3ad3f6f4133b306f416b054f0d5cc9f34d327a08
Ian Valentine <ian@ivalentine.net>
2019-09-12 12:11:08 -0400

    Create migrations

--- src/api/sql/migrations/V1566943365_create_people_table.sql
+++ src/api/sql/migrations/V1566943365_create_people_table.sql
@@ -1,13 +1,11 @@
-/**
- * Write SQL that creates a table named 'people'
- * it should match this schema
- * id BIGSERIAL PRIMARY KEY
- * first_name string with length 256 not null
- * last_name string with length 256 not null
- * birthday date not null
- * company string with length 256
- * title string with length 256
- * created_at timestamp with timezone not null defaults to now
- * updated_at timestamp with timezone
- * deleted_at timestamp with timezone
- ***/
+CREATE TABLE people (
+  id BIGSERIAL PRIMARY KEY,
+  first_name VARCHAR(256) NOT NULL,
+  last_name VARCHAR(256) NOT NULL,
+  birthday DATE NOT NULL,
+  company VARCHAR(256),
+  title VARCHAR(256),
+  created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+  updated_at TIMESTAMPTZ,
+  deleted_at TIMESTAMPTZ
+);
--- src/api/sql/migrations/V1566943370_create_addresses_table.sql
+++ src/api/sql/migrations/V1566943370_create_addresses_table.sql
@@ -1,14 +1,12 @@
-/**
- * Write SQL that creates a table named 'addresses'
- * it should match this schema
- * id BIGSERIAL PRIMARY KEY
- * person_id BIGINT not null Foreign Key to people.id
- * line1 string with length 256 not null
- * line2 string with length 256
- * city string with length 256 not null
- * state string with length 256 not null
- * zip string with length 256 not null
- * created_at timetsamp with timezone not null default now()
- * updated_at timetsamp with timezone
- * deleted_at timetsamp with timezone
- ***/
+CREATE TABLE addresses (
+  id BIGSERIAL PRIMARY KEY,
+  person_id BIGINT NOT NULL references people(id),
+  line1 VARCHAR(256) NOT NULL,
+  line2 VARCHAR(256),
+  city VARCHAR(256) NOT NULL,
+  state VARCHAR(256) NOT NULL,
+  zip VARCHAR(256) NOT NULL,
+  created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+  updated_at TIMESTAMPTZ,
+  deleted_at TIMESTAMPTZ
+);
