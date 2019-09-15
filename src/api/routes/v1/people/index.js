3672c73f6760b1df9a8d4edb8ba7dcaf73673441
Ian Valentine <ian@ivalentine.net>
2019-09-15 11:07:02 -0400

    First three endpoints + error handler

--- src/api/routes/v1/people/index.js
+++ src/api/routes/v1/people/index.js
@@ -8,19 +8,42 @@ module.exports = (api) => {
    * Create a new person
    */
   api.post('/', async (req, res, next) => {
-    res
-      .status(statusCodes.NotImplemented)
-      .json(httpErrorMessages.NotImplemented)
+    const results = await database('people')
+      .returning(['id', 'first_name', 'last_name',
+                  'title', 'company', 'birthday', 'created_at'])
+      .insert({
+        first_name: req.body.first_name,
+        last_name:  req.body.last_name,
+        title:      req.body.title,
+        company:    req.body.company,
+        birthday:   req.body.birthday,
+        created_at: req.body.created_at
+      })
+    if (results.length > 0) {
+      res.status(200).send(results[0])
+    } else {
+      const err = new Error('Unable to add new person');
+      err.statusCode = 500;
+      next(err);
+    }
   })
 
   /**
    * GET /v1/people/:personID
    * Retrieve a person by their ID
    */
-  api.get('/:personID', async (req, res) => {
-    res
-      .status(statusCodes.NotImplemented)
-      .json(httpErrorMessages.NotImplemented)
+  api.get('/:personID', async (req, res, next) => {
+    const results = await database('people')
+      .select(['id', 'first_name', 'last_name',
+              'title', 'company', 'birthday', 'created_at'])
+      .where({ id: req.params.personID })
+    if (results.length > 0) {
+      res.status(200).send(results[0])
+    } else {
+      const err = new Error('Person not found');
+      err.statusCode = 404;
+      next(err);
+    }
   })
 
   /**
@@ -28,9 +51,16 @@ module.exports = (api) => {
    * Retrieve a list of people
    */
   api.get('/', async (req, res) => {
-    res
-      .status(statusCodes.NotImplemented)
-      .json(httpErrorMessages.NotImplemented)
+    const results = await database('people')
+      .select(['id', 'first_name', 'last_name',
+              'title', 'company', 'birthday', 'created_at'])
+    if (results.length > 0) {
+      res.status(200).send(results)
+    } else {
+      const err = new Error('People not found');
+      err.statusCode = 404;
+      next(err);
+    }
   })
 
   /**
@@ -81,5 +111,13 @@ module.exports = (api) => {
     res
       .status(statusCodes.NotImplemented)
       .json(httpErrorMessages.NotImplemented)
+  })
+
+  api.use((err, req, res, next) => {
+    console.error(err);
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    res.status(err.statusCode).send(err.message);
   })
 }
