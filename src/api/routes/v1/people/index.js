7a4e16f9516c6b529127476dc2287205bc897fbe
Maryia Miller <maryiamiller@gmail.com>
2019-09-24 17:19:06 -0400

    finished node/express/knex

--- src/api/routes/v1/people/index.js
+++ src/api/routes/v1/people/index.js
@@ -1,37 +1,43 @@
-const statusCodes = require('../../../lib/httpStatusCodes')
-const httpErrorMessages = require('../../../lib/httpErrorMessages')
-const { database } = require('../../../lib/database')
+const statusCodes = require('../../../lib/httpStatusCodes');
+const httpErrorMessages = require('../../../lib/httpErrorMessages');
+const { database } = require('../../../lib/database');
 
-module.exports = (api) => {
+module.exports = api => {
   /**
    * POST /v1/people
    * Create a new person
    */
   api.post('/', async (req, res, next) => {
-    res
-      .status(statusCodes.NotImplemented)
-      .json(httpErrorMessages.NotImplemented)
-  })
+    try {
+      const data = await database('people').insert(req.body, '*');
+      res.status(200).json(data[0]);
+    } catch (error) {
+      next(error);
+    }
+  });
 
   /**
    * GET /v1/people/:personID
    * Retrieve a person by their ID
    */
   api.get('/:personID', async (req, res) => {
-    res
-      .status(statusCodes.NotImplemented)
-      .json(httpErrorMessages.NotImplemented)
-  })
+    try {
+      const data = await database('people').where('id', req.params.personID);
+      if (!data[0]) throw new Error('not found');
+      res.status(200).json(data[0]);
+    } catch (error) {
+      res.status(404).send(error.message);
+    }
+  });
 
   /**
    * GET /v1/people
    * Retrieve a list of people
    */
   api.get('/', async (req, res) => {
-    res
-      .status(statusCodes.NotImplemented)
-      .json(httpErrorMessages.NotImplemented)
-  })
+    const data = await database.select('*').from('people');
+    res.status(200).json(data);
+  });
 
   /**
    * Do not modify beyond this point until you have reached
@@ -47,8 +53,8 @@ module.exports = (api) => {
   api.post('/:personID/addresses', async (req, res) => {
     res
       .status(statusCodes.NotImplemented)
-      .json(httpErrorMessages.NotImplemented)
-  })
+      .json(httpErrorMessages.NotImplemented);
+  });
 
   /**
    * GET /v1/people/:personID/addresses/:addressID
@@ -57,8 +63,8 @@ module.exports = (api) => {
   api.get('/:personID/addresses/:addressID', async (req, res) => {
     res
       .status(statusCodes.NotImplemented)
-      .json(httpErrorMessages.NotImplemented)
-  })
+      .json(httpErrorMessages.NotImplemented);
+  });
 
   /**
    * GET /v1/people/:personID/addresses
@@ -67,8 +73,8 @@ module.exports = (api) => {
   api.get('/:personID/addresses', async (req, res) => {
     res
       .status(statusCodes.NotImplemented)
-      .json(httpErrorMessages.NotImplemented)
-  })
+      .json(httpErrorMessages.NotImplemented);
+  });
 
   /**
    * BONUS!!!!
@@ -80,6 +86,6 @@ module.exports = (api) => {
   api.delete('/:personID/addresses/:addressID', async (req, res) => {
     res
       .status(statusCodes.NotImplemented)
-      .json(httpErrorMessages.NotImplemented)
-  })
-}
+      .json(httpErrorMessages.NotImplemented);
+  });
+};
