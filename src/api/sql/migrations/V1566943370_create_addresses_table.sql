bf8217e78736081835b5727b76e42ed36377e2fa
Allison Geismar <ageismar213@gmail.com>
2019-09-30 10:13:06 -0400

    completed SQL specs

--- src/api/sql/migrations/V1566943370_create_addresses_table.sql
+++ src/api/sql/migrations/V1566943370_create_addresses_table.sql
@@ -12,3 +12,19 @@
  * updated_at timetsamp with timezone
  * deleted_at timetsamp with timezone
  ***/
+
+CREATE TABLE addresses
+(
+    id BIGSERIAL PRIMARY KEY,
+    person_id bigint NOT NULL,
+    line1 varchar(256) NOT NULL,
+    line2 varchar(256) NOT NULL,
+    city varchar(256) NOT NULL,
+    state varchar(256) NOT NULL,
+    zip varchar(256) NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ,
+    deleted_at TIMESTAMPTZ,
+    CONSTRAINT addresses_person_id_fkey FOREIGN KEY (person_id) REFERENCES people(id)
+
+)
