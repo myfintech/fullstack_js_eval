789658e90ca29b7a39c852840b4e1688fab8f909
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2019-09-30 15:43:14 -0400

    completed routing for people router moving on to TDD

--- src/api/routes/v1/people/index.js
+++ src/api/routes/v1/people/index.js
@@ -8,30 +8,58 @@ module.exports = (api) => {
    * Create a new person
    */
   api.post('/', async (req, res, next) => {
-    res
-      .status(statusCodes.NotImplemented)
-      .json(httpErrorMessages.NotImplemented)
-  })
+    try {
+      const newPerson = req.body;
+      await database('people')
+        .insert(newPerson, 'id')
+        .then(person => {
+          res.status(200).json({ id: person[0] });
+        });
+    } catch (error) {
+      next(error);
+    }
+  });
 
   /**
    * GET /v1/people/:personID
    * Retrieve a person by their ID
    */
   api.get('/:personID', async (req, res) => {
-    res
-      .status(statusCodes.NotImplemented)
-      .json(httpErrorMessages.NotImplemented)
-  })
+    try {
+      await database('people')
+        .where('id', req.params.personID)
+        .select()
+        .then(people => {
+          if (people.length) {
+            res.status(200).json({ id: people[0].id });
+          } else {
+            res
+              .status(404)
+              .json({
+                error: `Could not find person with id ${req.params.personID}`
+              });
+          }
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  });
 
   /**
    * GET /v1/people
    * Retrieve a list of people
    */
   api.get('/', async (req, res) => {
-    res
-      .status(statusCodes.NotImplemented)
-      .json(httpErrorMessages.NotImplemented)
-  })
+    try {
+      await database('people')
+        .select()
+        .then(people => {
+          res.status(200).json(people);
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  });
 
   /**
    * Do not modify beyond this point until you have reached
