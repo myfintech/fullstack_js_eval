205998d2fcf6fe885b0ac1ff9a9cd406a4b892ae
Cristian Martinez <2cristianmartinez@gmail.com>
2019-10-01 11:24:19 -0400

    feature: all tests passing

--- src/api/routes/v1/people/index.js
+++ src/api/routes/v1/people/index.js
@@ -1,6 +1,6 @@
-const statusCodes = require('../../../lib/httpStatusCodes')
-const httpErrorMessages = require('../../../lib/httpErrorMessages')
-const { database } = require('../../../lib/database')
+const statusCodes = require('../../../lib/httpStatusCodes');
+const httpErrorMessages = require('../../../lib/httpErrorMessages');
+const { database } = require('../../../lib/database');
 
 module.exports = (api) => {
   /**
@@ -10,7 +10,13 @@ module.exports = (api) => {
   api.post('/', async (req, res, next) => {
     try {
       const { first_name, last_name, birthday, company, title } = req.body;
-      const data = await database('people').insert({ first_name, last_name, birthday, company, title }, '*');
+      const data = await database('people').insert({
+        first_name,
+        last_name,
+        birthday,
+        company,
+        title
+      }, '*');
       res.status(200).json(data[0]);
     } catch (error) {
       next(error);
@@ -26,7 +32,7 @@ module.exports = (api) => {
       const personID = req.params.personID;
       const data = await database('people').where('id', personID);
       if (!data[0]) res.status(404).send('that person was not found.');
-      else res.status(200).json(data[0]);
+      res.status(200).json(data[0]);
     } catch (error) {
       next(error);
     }
@@ -56,31 +62,53 @@ module.exports = (api) => {
    * POST /v1/people/:personID/addresses
    * Create a new address belonging to a person
    **/
-  api.post('/:personID/addresses', async (req, res) => {
-    res
-      .status(statusCodes.NotImplemented)
-      .json(httpErrorMessages.NotImplemented)
-  })
+  api.post('/:personID/addresses', async (req, res, next) => {
+    try {
+      const { personID } = req.params;
+      const { line1, line2, city, state, zip } = req.body;
+      const data = await database('addresses')
+        .insert({ line1, line2, city, state, zip, person_id: personID })
+        .returning('*');
+      res.status(200).json(data[0]);
+    } catch (error) {
+      next(error);
+    }
+  });
 
   /**
    * GET /v1/people/:personID/addresses/:addressID
    * Retrieve an address by it's addressID and personID
    **/
-  api.get('/:personID/addresses/:addressID', async (req, res) => {
-    res
-      .status(statusCodes.NotImplemented)
-      .json(httpErrorMessages.NotImplemented)
-  })
+  api.get('/:personID/addresses/:addressID', async (req, res, next) => {
+    try {
+      const data = await database('addresses')
+        .where({
+          id: req.params.addressID,
+          person_id: req.params.personID,
+        })
+        .whereNull('deleted_at');
+      if (!data[0]) res.status(404).send('Not Found');
+      res.status(200).json(data[0]);
+    } catch (error) {
+      next(error);
+    }
+  });
 
   /**
    * GET /v1/people/:personID/addresses
    * List all addresses belonging to a personID
    **/
-  api.get('/:personID/addresses', async (req, res) => {
-    res
-      .status(statusCodes.NotImplemented)
-      .json(httpErrorMessages.NotImplemented)
-  })
+  api.get('/:personID/addresses', async (req, res, next) => {
+    try {
+      const data = await database('addresses')
+        .where('person_id', req.params.personID)
+        .whereNull('deleted_at');
+      if (!data.length) res.status(404).send('Not found.');
+      res.status(200).json(data);
+    } catch (error) {
+      next(error);
+    }
+  });
 
   /**
    * BONUS!!!!
@@ -89,9 +117,19 @@ module.exports = (api) => {
    * Set it's deleted_at timestamp
    * Update the previous GET endpoints to omit rows where deleted_at is not null
    **/
-  api.delete('/:personID/addresses/:addressID', async (req, res) => {
-    res
-      .status(statusCodes.NotImplemented)
-      .json(httpErrorMessages.NotImplemented)
-  })
-}
+  api.delete('/:personID/addresses/:addressID', async (req, res, next) => {
+    try {
+      await database('addresses')
+        .where({
+          id: req.params.addressID,
+          deleted_at: null
+        })
+        .update({
+          deleted_at: moment().toISOString()
+        });
+      res.sendStatus(200);
+    } catch (error) {
+      next(error);
+    }
+  });
+};
--- src/api/tests/people.spec.js
+++ src/api/tests/people.spec.js
@@ -44,9 +44,37 @@ describe('People API', () => {
    * ======================================================
    */
 
-  it('POST /v1/people/:personID/addresses should create a new address')
-  it('GET /v1/people/:personID/addresses/:addressID should return an address by its id and its person_id')
-  it('GET /v1/people/:personID/addresses should return a list of addresses belonging to the person by that id')
+  it('POST /v1/people/:personID/addresses should create a new address', async () => {
+    await client
+      .post(`/v1/people/${fixtures.firstPerson.id}/addresses`)
+      .send({
+        line1: '8080 Mantl Ave',
+        line2: '8080',
+        city: 'Brooklyn',
+        state: 'New York',
+        zip: '12345'
+      })
+      .expect('Content-Type', fixtures.contentTypes.json)
+      .expect(httpStatusCodes.OK)
+      .then(resp => {
+        fixtures.secondAddress = resp.body;
+      });
+  });
+  it('GET /v1/people/:personID/addresses/:addressID should return an address by its id and its person_id', async () => {
+    await client
+      .get(`/v1/people/${fixtures.firstPerson.id}/addresses/${fixtures.secondAddress.id}`)
+      .expect('Content-Type', fixtures.contentTypes.json)
+      .expect(httpStatusCodes.OK)
+  });
+  it('GET /v1/people/:personID/addresses should return a list of addresses belonging to the person by that id', async () => {
+    await client
+      .get(`/v1/people/${fixtures.firstPerson.id}/addresses`)
+      .expect('Content-Type', fixtures.contentTypes.json)
+      .expect(httpStatusCodes.OK)
+      .then(resp => {
+        expect(resp.body).to.have.lengthOf.above(0)
+      })
+  })
 
   // BONUS!!!
   it('DELETE /v1/people/:personID/addresses/:addressID should delete an address by its id (BONUS)')
