20fd5821b0aa21ec9631a823d922a6962621eb8a
Rebecca Rossman <rrossman25@gmail.com>
2019-10-02 18:01:33 -0400

    addresses table created and tests passing

--- src/api/sql/migrations/U1566943365_create_people_table.sql
+++ src/api/sql/migrations/U1566943365_create_people_table.sql
@@ -1,13 +1,1 @@
 DROP TABLE IF EXISTS people;
-
-CREATE people (
-    id          bigserial PRIMARY KEY,
-    first_name  char(256) NOT NULL,
-    last_name   char(256) NOT NULL,
-    birthday    date NOT NULL,
-    company     char(256),
-    title       char(256),
-    created_at  timestamp [ (p) ] with time zone NOT NULL DEFAULT now(),
-    updated_at  timestamp [ (p) ] with time zone,
-    deleted_at  timestamp [ (p) ] with time zone
-)
