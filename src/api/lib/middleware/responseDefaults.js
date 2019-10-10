a1db8ec14a8c306641163a8b6f4d1420bd6ad275
Wes Reid <wesley.reid@gmail.com>
2019-10-10 18:12:19 -0400

    Forgot to use existing status code module here

--- src/api/lib/middleware/responseDefaults.js
+++ src/api/lib/middleware/responseDefaults.js
@@ -1,10 +1,12 @@
+const httpStatusCodes = require('../httpStatusCodes')
+
 module.exports = (app) => {
   app.use(function(req, res, next) {
     res
       .set({
         'Content-Type': 'application/json'
       })
-      .status(200);
+      .status(httpStatusCodes.OK);
     next();
   });
 }
