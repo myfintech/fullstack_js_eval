8c325f397b921fd41e196d315aebbfc2f418788b
Kreig K. Zimmerman <myself@kreig.me>
2019-10-11 15:06:14 -0400

    finished tdd portion. bonus next.

--- src/api/routes/v1/people/index.js
+++ src/api/routes/v1/people/index.js
@@ -9,21 +9,20 @@ module.exports = (api) => {
    */
   api.post('/', async (req, res, next) => {
     database
-      .returning('id')
-      .insert({first_name: 'Xuan', last_name: 'Schmidt', birthday: '01/01/1970', company: 'MegaCorp Inc', title: 'Rude Bwoy'})
+      .returning(['id', 'first_name', 'last_name', 'birthday', 'company', 'title'])
+      .insert({first_name: 'Juan', last_name: 'Schmidt', birthday: '01/01/1970', company: 'MegaCorp Inc', title: 'Rude Bwoy'})
       .into('people')
-      .catch((err) => {
-          console.error(err)
-          res
-            .type('json')
-            .status(statusCodes.InternalServerError)
-            .json({ id: 'unavailable' })
-        })
       .then(postPerson => {
-          res
-            .type('json')
-            .status(statusCodes.OK)
-            .json({ id: postPerson })
+        res
+          .type('json')
+          .status(statusCodes.OK)
+          .json(postPerson[0])
+      })
+      .catch((err) => {
+        console.error(err)
+        res
+          .status(statusCodes.InternalServerError)
+          .end()
       })
   })
 
@@ -33,29 +32,26 @@ module.exports = (api) => {
    */
   api.get('/:personID', async (req, res) => {
     database
-      .select('id')
+      .select('id', 'first_name', 'last_name', 'birthday', 'company', 'title')
       .from('people')
       .where('id', req.params.personID)
-      .catch((err) => {
-          console.error(err)
+      .then(getPerson => {
+        if(getPerson.length) {
           res
             .type('json')
-            .status(statusCodes.InternalServerError)
-            .json({ result: 'unavailable' })
-        })
-      .then(getPerson => {
-          if(getPerson.length) {
-            arrId = [];
-            arrId.push(getPerson[0].id)
-            res
-              .type('json')
-              .status(statusCodes.OK)
-              .json({id: arrId})
-          } else {
-            res
-              .status(statusCodes.NotFound)
-              .end()
-          }
+            .status(statusCodes.OK)
+            .json(getPerson[0])
+        } else {
+          res
+            .status(statusCodes.NotFound)
+            .end()
+        }
+      })
+      .catch((err) => {
+        console.error(err)
+        res
+          .status(statusCodes.InternalServerError)
+          .end()
       })
   })
 
@@ -67,18 +63,17 @@ module.exports = (api) => {
     database
       .select('*')
       .from('people')
-      .catch((err) => {
-          console.error(err)
-          res
-            .type('json')
-            .status(statusCodes.InternalServerError)
-            .json({ result: 'unavailable' })
-        })
       .then(getPeople => {
-          res
-            .type('json')
-            .status(statusCodes.OK)
-            .send(getPeople)
+        res
+          .type('json')
+          .status(statusCodes.OK)
+          .send(getPeople)
+      })
+      .catch((err) => {
+        console.error(err)
+        res
+          .status(statusCodes.InternalServerError)
+          .end()
       })
   })
 
@@ -94,9 +89,22 @@ module.exports = (api) => {
    * Create a new address belonging to a person
    **/
   api.post('/:personID/addresses', async (req, res) => {
-    res
-      .status(statusCodes.NotImplemented)
-      .json(httpErrorMessages.NotImplemented)
+    database
+      .returning(['id', 'person_id', 'line1', 'line2', 'city', 'state', 'zip'])
+      .insert({person_id: req.params.personID, line1: '45 Main St', line2: 'Ste 404', city: 'Kennebunkport', state: 'ME', zip: '04046'})
+      .into('addresses')
+      .then(postAddress => {
+        res
+          .type('json')
+          .status(statusCodes.OK)
+          .json(postAddress[0])
+      })
+      .catch((err) => {
+        console.error(err)
+        res
+          .status(statusCodes.InternalServerError)
+          .end()
+      })
   })
 
   /**
@@ -104,9 +112,28 @@ module.exports = (api) => {
    * Retrieve an address by it's addressID and personID
    **/
   api.get('/:personID/addresses/:addressID', async (req, res) => {
-    res
-      .status(statusCodes.NotImplemented)
-      .json(httpErrorMessages.NotImplemented)
+    database
+      .select('id', 'person_id', 'line1', 'line2', 'city', 'state', 'zip')
+      .from('addresses')
+      .where({'id': req.params.addressID, 'person_id': req.params.personID })
+      .then(getAddress => {
+        if(getAddress.length) { 
+          res
+            .type('json')
+            .status(statusCodes.OK)
+            .json(getAddress[0])
+        } else {
+          res
+            .status(statusCodes.NotFound)
+            .end()
+        }
+      })
+      .catch((err) => {
+        console.error(err)
+        res
+          .status(statusCodes.InternalServerError)
+          .end()
+      })
   })
 
   /**
@@ -114,9 +141,22 @@ module.exports = (api) => {
    * List all addresses belonging to a personID
    **/
   api.get('/:personID/addresses', async (req, res) => {
-    res
-      .status(statusCodes.NotImplemented)
-      .json(httpErrorMessages.NotImplemented)
+    database
+      .select('id', 'person_id', 'line1', 'line2', 'city', 'state', 'zip')
+      .from('addresses')
+      .where({'person_id': req.params.personID})
+      .then(getAddresses => {
+        res
+          .type('json')
+          .status(statusCodes.OK)
+          .json(getAddresses)
+      })
+      .catch((err) => {
+        console.error(err)
+        res
+          .status(statusCodes.InternalServerError)
+          .end()
+      })
   })
 
   /**
