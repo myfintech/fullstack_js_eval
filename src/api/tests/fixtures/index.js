5932d29b3d2f4461c07c948d37d446abca4108b5
Catherine Lavin <cmlavin7@gmail.com>
2019-10-23 08:04:35 -0400

    Add tests

--- src/api/tests/fixtures/index.js
+++ src/api/tests/fixtures/index.js
@@ -24,9 +24,18 @@ const firstPerson = {
   created_at: moment().toISOString()
 }
 
+const firstAddress = {
+  line1: faker.address.streetAddress(),
+  city: faker.address.city(),
+  state: faker.address.state(),
+  zip: faker.address.zipCode(),
+  created_at: moment().toISOString()
+}
+
 module.exports = {
   contentTypes,
   firstPerson,
+  firstAddress,
   peopleSchema,
   addressesSchema,
   passingRootServerResponse,
